0a570578cef1a1ee8f4b3aa17292e10354647224
Gowrishankarnagarajan <gowrishankarn.2096@gmail.com>
2025-07-25 10:58:31 +0100

    Docker image build

--- terraform/infra/output.tf
+++ terraform/infra/output.tf
@@ -1,6 +1,4 @@
-output "container_app_principal_id" {
-  value = azurerm_container_app.ingestion.identity[0].principal_id
-}
+
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
