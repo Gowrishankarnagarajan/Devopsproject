3a43675b5b7f6e2415155d403b23d28c1f4ce8e8
Gowrishankarnagarajan <gowrishankarn.2096@gmail.com>
2025-07-25 15:30:33 +0100

    Devops

--- terraform/apps/variables.tf
+++ terraform/apps/variables.tf
@@ -8,3 +8,10 @@ variable "location" {
   type    = string
   default = "UK South"
 }
+variable "acr_login_server" {
+  description = "The login server for the Azure Container Registry."
+  type        = string
+  # You can optionally add a default value or make it sensitive if needed.
+  # default     = "yourdefault.azurecr.io"
+  # sensitive   = true
+}
